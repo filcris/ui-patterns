81afc1572f007aa98bd7065b562b8277c66104dc
filcris <filcris@users.noreply.github.com>
2025-11-03 13:35:22 +0000

    deploy: d246e8e3d5ca295080928dcc6444f1e64a7e501c

---
+++ assets/Button.stories-CMG945u5.js
@@ -0,0 +1,16 @@
+import{B as l}from"./Button-BTyCKOgM.js";import"./jsx-runtime-CDt2p4po.js";import"./index-GiUgBvb1.js";import"./clsx-B-dksMZM.js";const v={title:"Components/Button",component:l,tags:["autodocs"],argTypes:{variant:{control:"select",options:["primary","secondary","ghost"]},children:{control:"text"}}},r={args:{children:"Primary",variant:"primary"}},a={args:{children:"Secondary",variant:"secondary"}},o={args:{children:"Ghost",variant:"ghost"}};var e,s,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    children: "Primary",
+    variant: "primary"
+  }
+}`,...(t=(s=r.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var n,c,i;a.parameters={...a.parameters,docs:{...(n=a.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    children: "Secondary",
+    variant: "secondary"
+  }
+}`,...(i=(c=a.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};var m,d,p;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: "Ghost",
+    variant: "ghost"
+  }
+}`,...(p=(d=o.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};const S=["Primary","Secondary","Ghost"];export{o as Ghost,r as Primary,a as Secondary,S as __namedExportsOrder,v as default};
---
+++ assets/Input.stories-Hts9Q_oS.js
@@ -0,0 +1,12 @@
+import{I as s}from"./Input-CLUMq6E9.js";import"./jsx-runtime-CDt2p4po.js";import"./index-GiUgBvb1.js";const d={title:"Components/Input",component:s,parameters:{layout:"centered"},argTypes:{label:{control:"text"},placeholder:{control:"text"},error:{control:"text"}}},e={args:{label:"Email",placeholder:"exemplo@dominio.pt"}},r={args:{label:"Email",placeholder:"exemplo@dominio.pt",error:"Endereço de email inválido."}};var o,a,t;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    label: "Email",
+    placeholder: "exemplo@dominio.pt"
+  }
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var l,n,p;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: "Email",
+    placeholder: "exemplo@dominio.pt",
+    error: "Endereço de email inválido."
+  }
+}`,...(p=(n=r.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};const u=["Default","WithError"];export{e as Default,r as WithError,u as __namedExportsOrder,d as default};
